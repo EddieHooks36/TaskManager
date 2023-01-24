93ee5c1459bd83a4f6e36fd2a93cc98dcb850b56
EddieHooks36 <eddiehooks36@gmail.com>
2023-01-23 21:39:57 -0800

    css flex

--- scripts/script.js
+++ scripts/script.js
@@ -34,7 +34,30 @@ function saveTask(){
     let status = $("#selStatus").val();
 
     let task = new Task(title, description, dueDate, category, contact, status, isImportant);
+    //save logic
 
+    //display logic
+    displayTask(task); //calling the task
+}
+function displayTask(task){
+     let syntax = `<div class="task">
+        <div>
+            <h3>${task.title}</h3>
+            <p>${task.description}</p>
+        </div>
+
+        <div>
+            <label>${task.dueDate}</label>
+            <label>${task.category}</label>
+        </div>
+    
+        <div>
+            <label>${task.contact}</label>
+            <label>${task.status}</label>
+        </div>
+
+    </div>`
+     $(".list-container").append(syntax);
 }
 function init(){
     console.log("Task Manager");
