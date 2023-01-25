cd915e1ac9fa205be267a030aea88bd3aaaf5b0a
EddieHooks36 <eddiehooks36@gmail.com>
2023-01-24 19:43:53 -0800

    106 class 4

--- scripts/script.js
+++ scripts/script.js
@@ -35,23 +35,37 @@ function saveTask(){
 
     let task = new Task(title, description, dueDate, category, contact, status, isImportant);
     //save logic
+    $.ajax({
+         type: "POST",
+         url:"https://fsdiapi.azurewebsites.net/api/tasks",
+         data: JSON.stringify(task),
+         contentType: "application/json",
+         success: function(res){
+             console.log(res); //res is a JSON string
 
-    //display logic
-    displayTask(task); //calling the task
+               //display logic
+             displayTask(task); //calling the task
+         },
+         error: function(error){
+            console.log(error);
+
+            alert("Unexpected Error")
+         }
+    }); 
 }
 function displayTask(task){
      let syntax = `<div class="task">
-        <div>
+        <div class="col1">
             <h3>${task.title}</h3>
             <p>${task.description}</p>
         </div>
 
-        <div>
+        <div class="col2">
             <label>${task.dueDate}</label>
             <label>${task.category}</label>
         </div>
     
-        <div>
+        <div class="col3">
             <label>${task.contact}</label>
             <label>${task.status}</label>
         </div>
@@ -59,10 +73,46 @@ function displayTask(task){
     </div>`
      $(".list-container").append(syntax);
 }
+function testRequest(){
+    $.ajax({
+        type: "GET",
+        url:"https://fsdiapi.azurewebsites.net/",
+        success: function(response){
+            console.log(response);
+
+        },
+        error: function(error){
+            console.log(error);
+        }
+    });
+}
+function loadTasks(){
+    $.ajax({
+        type: "GET",
+        url: "https://fsdiapi.azurewebsites.net/api/tasks",
+        success: function(res){
+            let data = JSON.parse(res); //will parse the JSON string into JS objects
+            console.log(data);
+            for(let i=0; i<data.length; i++){
+                let task = data[i]; //get every object
+                
+                if(task.name  == "Eddie"){
+                displayTask(task);
+            }
+            }
+        },
+        error: function(error){
+            console.log(error);
+        }
+
+    })
+   
+}
 function init(){
     console.log("Task Manager");
 
     // loads data
+    loadTasks();
 
     // assigns events
     $("#iImportant").click(toggleImportant);
